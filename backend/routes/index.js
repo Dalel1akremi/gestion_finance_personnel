eb992fe4b570769a7b2b37a1bbab51a52db1aa27
Dalel1akremi <dalelakremi2020@gmail.com>
2023-09-08 10:02:29 +0100

    feat: add `Historique` router in `index` js file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {Login,Register,AjoutDepense,getRecentDepenses} from "../controllers/User.js"
+import {Login,Register,AjoutDepense,getRecentDepenses, Historique} from "../controllers/User.js"
 import { verifyToken } from "../middelware/VerifyToken.js";
 const router = express.Router();
 
@@ -7,5 +7,7 @@ router.post('/Login', Login);
 router.post('/Register', Register);
 router.post('/AjoutDepense',AjoutDepense);
 router.get('/recentDepenses',getRecentDepenses)
+router.post('/AjoutDepense',AjoutDepense)
+router.get('/Historique',Historique)
 // if make private route verify token user before send response must call to verifyToken feature with the intial feature for this route
 export default router;

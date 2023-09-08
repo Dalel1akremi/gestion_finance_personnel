94b6b2b4db1d282ec5f71f3a46c3fa39b0e1e0dc
Dalel1akremi <dalelakremi2020@gmail.com>
2023-09-08 14:53:48 +0100

    chore cleanup

--- frontend/src/components/Historique.jsx
+++ frontend/src/components/Historique.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Historique.css';
-import { colours } from 'nodemon/lib/config/defaults';
+
 
 
 function Historique() {

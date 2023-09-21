e80b6a15a94fedd5df42ec952100793de18610f9
Dalel1akremi <dalelakremi2020@gmail.com>
2023-09-21 12:02:05 +0100

    feat: add `verifyToken` middelware

--- backend/middelware/VerifyToken.js
+++ backend/middelware/VerifyToken.js
@@ -1,12 +1,26 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if(token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if(err) return res.sendStatus(403);
-        req.email = decoded.email;
-        next();
-    })
+// jwtMiddleware.js
+console.log("test 1")
+
+const authHeader = req.headers['authorization'];
+const token = authHeader && authHeader.split(' ')[1];
+
+console.log("data: ", process.env.ACCESS_TOKEN_SECRET)
+  
+  if (token.length === 0) {
+    return res.status(401).json({ message: 'JWT must be provided' });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    
+    if (err) {
+        return res.status(401).json({ message: err.message });
+    } 
+    console.log("user: ", decoded)
+    req.user = decoded; // Attach user information to the request object
+    next();
+  });
 }
+
+

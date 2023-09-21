b0824ae80e297200083452827266346eb33d66fc
abir <abirghrissi83@gmail.com>
2023-09-21 16:30:59 +0200

    db modification

--- backend/basededonée.sql
+++ backend/basededonée.sql
@@ -2,85 +2,78 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 -- Table structure for table `authentification`
---
 
-
-CREATE TABLE Users(
-   id int(11),
-   firstName VARCHAR(50) ,
-   lastName VARCHAR(50) ,
-   email VARCHAR(50) unique,
+CREATE TABLE Users (
+   id INT(11) NOT NULL AUTO_INCREMENT,
+   firstName VARCHAR(50),
+   lastName VARCHAR(50),
+   email VARCHAR(50) UNIQUE,
    password VARCHAR(255),
    createdAt DATETIME NOT NULL,
    updatedAt DATETIME NOT NULL,
-   PRIMARY KEY(id,email)
+   PRIMARY KEY (id, email)
 );
 
---
 -- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `Users`
-  MODIFY `id_user` NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
-
-
-
-CREATE TABLE categories(
-   id_cat VARCHAR(50),
-  nom VARCHAR(50) unique,
+CREATE TABLE categories (
+   id_cat INT(11) NOT NULL AUTO_INCREMENT,
+   nom_cat VARCHAR(50) UNIQUE,
    createdAt DATETIME NOT NULL,
    updatedAt DATETIME NOT NULL,
-   PRIMARY KEY(id_cat)
+   id INT(11),
+   PRIMARY KEY (id_cat),
+   FOREIGN KEY (id) REFERENCES Users (id)
 );
 
-
---
 -- AUTO_INCREMENT for table `categories`
---
 ALTER TABLE `categories`
-  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id_cat` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
+INSERT INTO categories (nom) VALUES ('Vétements');
+INSERT INTO categories (nom) VALUES ('Aliment');
+INSERT INTO categories (nom) VALUES ('Sport');
+INSERT INTO categories (nom) VALUES ('Autres Dépenses');
 
-
-  CREATE TABLE depenses(
-   id_dep varchar(50),
-   id int(11) ,
-   id_cat int(11) ,
+CREATE TABLE depenses (
+   id_dep INT(11) NOT NULL AUTO_INCREMENT,
+   id INT(11),
+   id_cat INT(11),
    Montant DOUBLE NOT NULL,
-   Categorie varchar(50),
-   Date date NOT NULL,
+   Categorie VARCHAR(50),
+   Date DATE NOT NULL,
    Description VARCHAR(500) NOT NULL,
    createdAt DATETIME NOT NULL,
    updatedAt DATETIME NOT NULL,
-   FOREIGN KEY (id) REFERENCES Users(id),
-    FOREIGN KEY (id_cat) REFERENCES categories(id_cat),
-   PRIMARY KEY(id_dep)
+   PRIMARY KEY (id_dep),
+   FOREIGN KEY (id) REFERENCES Users (id),
+   FOREIGN KEY (id_cat) REFERENCES categories (id_cat)
 );
--- AUTO_INCREMENT for table `des depenses`--
+
+-- AUTO_INCREMENT for table `depenses`
 ALTER TABLE `depenses`
-  MODIFY `id_dep` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id_dep` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
-
-
-CREATE TABLE revenues(
-   id_rev VARCHAR(50),
-   email int(11),
+CREATE TABLE revenues (
+   id_rev INT(11) NOT NULL AUTO_INCREMENT,
+   email INT(11),
    Montant DOUBLE NOT NULL,
-   Date date NOT NULL,
+   Date DATE NOT NULL,
    Description VARCHAR(500) NOT NULL,
+   id INT(11),
    createdAt DATETIME NOT NULL,
    updatedAt DATETIME NOT NULL,
-   FOREIGN KEY (id_user)REFERENCES Users(id_user) ,
-   PRIMARY KEY(id_rev)
+   PRIMARY KEY (id_rev),
+   FOREIGN KEY (id) REFERENCES Users (id)
 );
 
---
 -- AUTO_INCREMENT for table `revenues`
---
 ALTER TABLE `revenues`
-  MODIFY `id_rev` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-  
+  MODIFY `id_rev` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;

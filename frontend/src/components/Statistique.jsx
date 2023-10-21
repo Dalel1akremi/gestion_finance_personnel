5e743c37c310aa736a93080641a51526c5f01309
Dalel1akremi <dalelakremi2020@gmail.com>
2023-10-21 23:03:51 +0100

    feat:fix`statistique` not finish

--- frontend/src/components/Statistique.jsx
+++ frontend/src/components/Statistique.jsx
@@ -5,67 +5,46 @@ import CanvasJSReact from '@canvasjs/react-charts';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { Bar } from 'react-chartjs-2';
-import {
-  Chart,
-  CategoryScale,
-  LinearScale,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js/auto';
-
-// Enregistrez les échelles nécessaires
-Chart.register(LinearScale, CategoryScale, Title, Tooltip, Legend);
-  
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const ExpenseStatistics = () => {
   const [data, setData] = useState({
     revenueData: [],
     expenseData: [],
-    chartData: {
-      labels: [],
-      datasets: [
-        {
-          label: 'Depense par Categorie',
-          data: [],
-          backgroundColor: ["blueviolet","greenyellow","rgb(255, 0, 212)","rgb(0, 255, 255)","yellow"]
-        },
-      ],
-    },
   });
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
+  const [error, setError] = useState(null);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
+
+  // Define options for charts here
+  
+
   const fetchData = async () => {
     setLoading(true);
-  
+
     try {
       const token = localStorage.getItem("token");
-      const response = await axios.get('http://localhost:5000/Acceuil', {
+      const response = await axios.get('http://localhost:5000/statistics', {
         params: {
           startDate: startDate?.toISOString(),
           endDate: endDate?.toISOString(),
         },
         headers: { "Authorization": `Bearer ${token}` }
       });
-  
-      if (response.data && response.data.Revenue && response.data.Depense) {
-        const revenueData = response.data.Revenue.map((item) => ({
-          x: new Date(item.Date),
-          y: item.Total,
-        }));
-  
-        const expenseData = response.data.Depense.map((item) => ({
-          x: new Date(item.Date),
-          y: item.Total,
-        }));
-  
-        setData({ revenueData, expenseData });
-      } else {
-        // Gérer le cas où les données sont indéfinies ou incorrectes
-      }
+
+      const categories = response.data.map((entry) => entry.Categorie);
+      const totalAmounts = response.data.map((entry) => entry.Total);
+
+      setData({
+        labels: categories,
+        datasets: [
+          {
+            label: 'Depense par Categorie',
+            data: totalAmounts,
+            backgroundColor: ["blueviolet", "greenyellow", "rgb(255, 0, 212)", "rgb(0, 255, 255)", "yellow"]
+          },
+        ],
+      });
     } catch (error) {
       console.error('Error fetching data:', error.message);
       setError(error.message);
@@ -74,8 +53,6 @@ const ExpenseStatistics = () => {
     }
   };
 
-
-  
   const fetch = async () => {
     setLoading(true);
 
@@ -112,7 +89,6 @@ const ExpenseStatistics = () => {
     fetchData();
     fetch();
   }, [startDate, endDate]);
-
   const optionsRevenue = {
     title: {
       text: '',
@@ -150,9 +126,29 @@ const ExpenseStatistics = () => {
       },
     ],
   };
-
   return (
     <div>
+      <div className="statistic_container">
+        <div className="graphic">
+          <h1>Statistique des depenses par rapport a categories</h1>
+          <div className='date-filters'>
+            <h6>Plage de date:</h6>
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              placeholderText="Select start date"
+            />
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+              placeholderText="Select end date"
+            />
+          </div>
+          <div style={{ width: '80%', margin: 'auto' }}>
+            {data.labels && data.labels.length > 0 ? <Bar data={data} /> : null}
+          </div>
+        </div>
+      </div>
       <div className="statistic_container">
         <div className="graphic">
           <h1>Revenues par rapport au temps</h1>
@@ -170,10 +166,10 @@ const ExpenseStatistics = () => {
             />
           </div>
           <div style={{ width: '80%', margin: 'auto' }}>
-  {data.revenueData && data.revenueData.length > 0  ? (
-    <CanvasJSChart options={optionsRevenue} />
-  ) : null}
-</div>
+            {data.revenueData && data.revenueData.length > 0 ? (
+              <CanvasJSReact.CanvasJSChart options={optionsRevenue} />
+            ) : null}
+          </div>
         </div>
       </div>
       <div className="statistic_container">
@@ -193,36 +189,11 @@ const ExpenseStatistics = () => {
             />
           </div>
           <div style={{ width: '80%', margin: 'auto' }}>
-  {data.expenseData && data.expenseData.length > 0  ? (
-    <CanvasJSChart options={optionsDepense} />
-  ) : null}
-</div>
-        </div>
-        <div className="statistic_container">
-        <div className="graphic">
-          <h1>Statistique des depenses par rapport a categories</h1>
-          {/* Date picker for start date */}
-          <div className='date-filters'>
-            <h6>Plage de date:</h6>
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              placeholderText="Select start date"
-            />
-            {/* Date picker for end date */}
-            <DatePicker
-              selected={endDate}
-              onChange={(date) => setEndDate(date)}
-              placeholderText="Select end date"
-            />
-          </div>
-          <div style={{ width: '80%', margin: 'auto' }}>
-          {data.chartData && data.chartData.length > 0  ?  (
-              <Bar data={data.chartData} />
+            {data.expenseData && data.expenseData.length > 0 ? (
+              <CanvasJSReact.CanvasJSChart options={optionsDepense} />
             ) : null}
           </div>
         </div>
-      </div>
       </div>
     </div>
   );

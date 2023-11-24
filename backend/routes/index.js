2e2513de1a72ff23e3d988c9000d0942d058e0fd
yakinebenali <yakinebenali5@gmail.com>
2023-11-24 21:36:34 +0100

    feat:add `statistique `page

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {Login,Register,AjoutDepense,Acceuil, Historique, Email, Statistique,getCategories,addCategory,editCategory,deleteCategory,reset_password,changePassword,checkUnaddedExpenses,MontantAcceuil} from "../controllers/User.js"
+import {Login,Register,AjoutDepense, Historique, Email, Statistique,getCategories,addCategory,editCategory,deleteCategory,reset_password,changePassword,checkUnaddedExpenses,MontantAcceuil} from "../controllers/User.js"
 import { verifyToken } from "../middelware/VerifyToken.js";
 const router = express.Router();
 
@@ -8,7 +8,7 @@ router.post('/Register', Register);
 router.post('/AjoutDepense', verifyToken, AjoutDepense);
 router.get('/Historique', verifyToken,Historique)
 router.post('/Contact', Email)
-router.get('/Acceuil', verifyToken, Acceuil)
+
 router.get('/statistics', verifyToken, Statistique)
 // if make private route verify token user before send response must call to verifyToken feature with the intial feature for this route
 router.get('/getCategories', verifyToken, getCategories) 
--- frontend/src/components/Statistique.jsx
+++ frontend/src/components/Statistique.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './acceuil.css';
-import './Statistique.css';
+import './Historique.css';
+import { MDBTabs, MDBTabsItem, MDBTabsLink, MDBTabsPane } from 'mdb-react-ui-kit';
 import CanvasJSReact from '@canvasjs/react-charts';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import {
   Chart,
   CategoryScale,
@@ -11,66 +13,73 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js/auto';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import { Bar } from 'react-chartjs-2';
+
+// Register the necessary scales
 Chart.register(LinearScale, CategoryScale, Title, Tooltip, Legend);
-const ExpenseStatistics = () => {
-  const [data, setData] = useState({
-    revenueData: [],
-    expenseData: [],
-      labels: [],
-      datasets: [
-        {
-          label: 'Dépense par Catégorie',
-          data: [],
-          backgroundColor: ["blueviolet", "greenyellow", "rgb(255, 0, 212)", "rgb(0, 255, 255)", "yellow"],
-        },
-      ],
-    });
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+function Statistique() {
+  const [depense, setDepense] = useState([]);
+  const [revune, setRevune] = useState([]);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
+  const [justifyActive, setJustifyActive] = useState('tab1');
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  // Define options for charts here
-  
- 
-  const fetch = async () => {
-    setLoading(true);
-
-    try {
-      const token = localStorage.getItem("token");
-      const response = await axios.get('http://localhost:5000/Acceuil', {
-        params: {
-          startDate: startDate?.toISOString(),
-          endDate: endDate?.toISOString(),
-        },
-        headers: { "Authorization": `Bearer ${token}`}
-      });
-
-      const revenueData = response.data.Revenue.map((item) => ({
-        x: new Date(item.Date),
-        y: item.Total,
-      }));
-
-      const expenseData = response.data.Depense.map((item) => ({
-        x: new Date(item.Date),
-        y: item.Total,
-      }));
-
-      setData({ revenueData, expenseData });
-    } catch (error) {
-      console.error('Error fetching data:', error.message);
-      setError(error.message);
-    } finally {
-      setLoading(false);
+  const handleJustifyClick = (value) => {
+    if (value === justifyActive) {
+      return;
     }
+    setJustifyActive(value);
   };
-  
+
   useEffect(() => {
-    fetch(); // Appeler la fonction fetch
-  }, [startDate, endDate]);
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const token = localStorage.getItem('token');
+        const response = await axios.get('http://localhost:5000/statistics', {
+          params: {
+            startDate: startDate?.toISOString(),
+            endDate: endDate?.toISOString(),
+            type: justifyActive === 'tab2' ? 'Depense' : 'Revenue',
+          },
+          headers: { "Authorization": `Bearer ${token}` },
+        });
+
+        if (response.data && response.data.Revenue && response.data.Depense) {
+          const revenueData = response.data.Revenue.map((item) => ({
+            x: new Date(item.Date),
+            y: item.Total,
+          }));
+
+          const expenseData = response.data.Depense.map((item) => ({
+            x: new Date(item.Date),
+            y: item.Total,
+          }));
+
+          setData({ revenueData, expenseData });
+          console.log("Data set:", { revenueData, expenseData });
+        } else {
+          // Handle the case where the data is undefined or incorrect
+          console.log("Data is undefined or incorrect:", response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error.message);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [startDate, endDate, justifyActive]);
+
+  console.log("Data before rendering:", data);
+
   const optionsRevenue = {
     title: {
       text: '',
@@ -86,11 +95,10 @@ const ExpenseStatistics = () => {
         type: 'line',
         name: 'Revenue',
         showInLegend: true,
-        dataPoints: data.revenueData,
+        dataPoints: data && data.revenueData ? data.revenueData : [],
       },
     ],
   };
-  
   const optionsDepense = {
     title: {
       text: '',
@@ -101,72 +109,26 @@ const ExpenseStatistics = () => {
     axisY: {
       title: 'Montant',
     },
-    data: [
+    data:[
       {
-        type: 'line',
-        name: 'Dépenses',
-        showInLegend: true,
-        dataPoints: data.expenseData,
-      },
-    ],
-  };
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const token = localStorage.getItem("token");
-
-        const response = await axios.get('http://localhost:5000/statistics', {
-          params: {
-            startDate: startDate?.toISOString(),
-            endDate: endDate?.toISOString(),
-          },
-          headers: { "Authorization": `Bearer ${token}` }
-        });
-
-        const categories = response.data.map((entry) => entry.Categorie);
-        const totalAmounts = response.data.map((entry) => entry.Total);
-
-        setData({
-          labels: categories,
-          datasets: [
-            {
-              label: 'Dépense par Catégorie',
-              data: totalAmounts,
-              backgroundColor: ["blueviolet", "greenyellow", "rgb(255, 0, 212)", "rgb(0, 255, 255)", "yellow"],
-            },
-          ],
-        });
-      } catch (error) {
-        console.error('Error fetching data:', error.message);
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  },  [startDate, endDate]);
-
-  const options = {
-    indexAxis: 'y',
-    elements: {
-      bar: {
-        borderWidth: 2,
-      },
+      type: 'line',
+      name: 'Dépenses',
+      showInLegend: true,
+      dataPoints: data.expenseData,
     },
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Dépense par Catégorie',
-      },
-    },
+  ],
+};
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
   };
+
+  const handleEndDateChange = (date) => {
+    setEndDate(date);
+  };
+
   return (
     <div>
+          
           <header>
 			<nav>
       <ul className="navbar"><li className="logo" >Gestion de Finance Personnelle</li>			  <li><a href="/acceuil">Acceuil</a></li>
@@ -201,75 +163,60 @@ const ExpenseStatistics = () => {
 			 </ul>
 			</nav>
 		  </header>
-      <div className="statistic_container">
-        <div className="graphic">
-          <h1>Statistique des depenses par rapport a categories</h1>
-          <div className='date-filters'>
-            <h6>Plage de date:</h6>
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              placeholderText="Select start date"
-            />
-            <DatePicker
-              selected={endDate}
-              onChange={(date) => setEndDate(date)}
-              placeholderText="Select end date"
-            />
-          </div>
-          <div style={{ width: '80%', margin: 'auto' }}>
-            {data !== undefined ? <Bar data={data} /> : null}
-          </div>
-        </div>
-      </div>
-      <div className="statistic_container">
-        <div className="graphic">
-          <h1>Revenues par rapport au temps</h1>
-          <div className='date-filters'>
-            <h6>Plage de date:</h6>
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              placeholderText="Select start date"
-            />
-            <DatePicker
-              selected={endDate}
-              onChange={(date) => setEndDate(date)}
-              placeholderText="Select end date"
-            />
-          </div>
-          <div style={{ width: '80%', margin: 'auto' }}>
-            {data.revenueData && data.revenueData.length > 0 ? (
-              <CanvasJSReact.CanvasJSChart options={optionsRevenue} />
-            ) : null}
-          </div>
-        </div>
-      </div>
-      <div className="statistic_container">
-        <div className="graphic">
-          <h1>Depenses par rapport au temps</h1>
-          <div className='date-filters'>
-            <h6>Plage de date:</h6>
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              placeholderText="Select start date"
-            />
-            <DatePicker
-              selected={endDate}
-              onChange={(date) => setEndDate(date)}
-              placeholderText="Select end date"
-            />
-          </div>
-          <div style={{ width: '80%', margin: 'auto' }}>
-            {data.expenseData && data.expenseData.length > 0 ? (
-              <CanvasJSReact.CanvasJSChart options={optionsDepense} />
-            ) : null}
-          </div>
+      <div className="essential-section">
+        <div className="middle-section">
+          <MDBTabs>
+            <MDBTabsItem>
+              <MDBTabsLink onClick={() => handleJustifyClick('tab1')} active={justifyActive === 'tab1'}>
+                Revenue
+              </MDBTabsLink>
+            </MDBTabsItem>
+            <MDBTabsItem>
+              <MDBTabsLink onClick={() => handleJustifyClick('tab2')} active={justifyActive === 'tab2'}>
+                Depense
+              </MDBTabsLink>
+            </MDBTabsItem>
+          </MDBTabs>
+          {justifyActive === 'tab1' && (
+            <MDBTabsPane show={justifyActive === 'tab1'}>
+              <h2 id="h">Statistique :</h2>
+              <div className="date-filters">
+                <h6>Plage de date:</h6>
+                <DatePicker selected={startDate} onChange={handleStartDateChange} placeholderText="Start Date" />
+                <DatePicker selected={endDate} onChange={handleEndDateChange} placeholderText="End Date" />
+              </div>
+
+              <div style={{ width: '80%', margin: 'auto' }}>
+                {data && data.revenueData && data.revenueData.length > 0 ? (
+                  <CanvasJSChart options={optionsRevenue} />
+                ) : null}
+              </div>
+
+              {(!data || !data.revenueData || data.revenueData.length === 0) && <p>No data to display.</p>}
+            </MDBTabsPane>
+          )}
+           {justifyActive === 'tab2' && (
+            <MDBTabsPane show={justifyActive === 'tab2'}>
+              <h2 id="h">Statistique :</h2>
+              <div className="date-filters">
+                <h6>Plage de date:</h6>
+                <DatePicker selected={startDate} onChange={handleStartDateChange} placeholderText="Start Date" />
+                <DatePicker selected={endDate} onChange={handleEndDateChange} placeholderText="End Date" />
+              </div>
+
+              <div style={{ width: '80%', margin: 'auto' }}>
+                {data && data.expenseData && data.expenseData.length > 0 ? (
+                  <CanvasJSChart options={optionsDepense} />
+                ) : null}
+              </div>
+
+              {(!data || !data.expenseData || data.expenseData.length === 0) && <p>No data to display.</p>}
+            </MDBTabsPane>
+          )}
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default ExpenseStatistics;
+export default Statistique;
